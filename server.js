e60564519c4151bcfd5641dfd3e0d2aba75903fc
batty93 <batz.elizabeth11@gmail.com>
2025-10-23 12:07:02 -0400

    Added 2 more pages of html for this project

--- server.js
+++ server.js
@@ -14,3 +14,28 @@ app.listen(port, () => {
     console.log("Press Ctrl+C to end this process.");
 });
 
+
+
+const apiKey = 'ef09a2977e954f2786bdcc657c7995a4';
+const apiUrl = `https://api.currencyfreaks.com/v2.0/rates/latest?apikey=ef09a2977e954f2786bdcc657c7995a4`;
+
+const requestOptions = {
+    method: 'GET',
+    headers: {
+        'Authorization': `Bearer ${apiKey}`,
+    },
+};
+
+fetch(apiUrl, requestOptions)
+    .then(response => {
+        if !response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        outputElement.textContent = JSON.stringify(data, null, 2);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
